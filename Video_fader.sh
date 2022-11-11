9b5a364446f61abea78907516b5933cb812183b3
Sadra <sadra.m@radskunkworks.com>
2022-11-11 13:53:37 -0500

    voice  changer added

--- Video_fader.sh
+++ Video_fader.sh
@@ -4,4 +4,7 @@ echo "The directory is: $1."
 rm ./result/*
 mkdir -p result
 for FILE in $1*; do echo $FILE; done
-for FILE in $1*; do ffmpeg -i $FILE -vf "boxblur=10" -c:a copy ./result/blurred_video.mp4 && stat $FILE > ./result/meta_data.txt; done
+for FILE in $1*; do ffmpeg -i $FILE -vf "boxblur=10" -c:a copy ./result/blurred_video.mp4 && \
+stat $FILE > ./result/meta_data.txt && \
+ffmpeg -i ./result/blurred_video.mp4 -af asetrate=44100*3/4,atempo=4/3 ./result/voice_and_video_changed.mp4; done
+rm ./result/blurred_video.mp4
