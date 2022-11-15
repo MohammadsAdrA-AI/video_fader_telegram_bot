e8627cc844874c3250d090c0385f575a28b105ea
Sadra <sadra.m@radskunkworks.com>
2022-11-15 10:02:28 -0500

    rewrite added

--- Video_fader.sh
+++ Video_fader.sh
@@ -4,7 +4,7 @@ echo "The directory is: $1."
 rm ./result/*
 mkdir -p result
 for FILE in $1*; do echo $FILE; done
-for FILE in $1*; do ffmpeg -i $FILE -vf "boxblur=5" -c:a copy ./result/blurred_video.mp4 && \
+for FILE in $1*; do ffmpeg -i $FILE -vf "boxblur=5" -c:a copy ./result/blurred_video.mp4 -y && \
 stat $FILE > ./result/meta_data.txt && \
 ffmpeg -i ./result/blurred_video.mp4 -af asetrate=44100*3/4,atempo=4/3 ./result/voice_and_video_changed.mp4; done
 rm ./result/blurred_video.mp4
